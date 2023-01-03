a67e2d6e1e9667819538b17c3a076638a7fb39ba
yuiseki <yuiseki@gmail.com>
2023-01-03 16:47:06 +0900

    fix fetcher

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,1 +1,4 @@
-export const fetcher = (url: string) => fetch(url).then((res) => res.json());
+export const fetcher = (url: string) =>
+  fetch(url).then((res) =>
+    res.headers.get("content-type")?.includes("json") ? res.json() : res.text()
+  );
